eae86c0afeecf2cb63be199f0ec9f8c1462e785c
emarinfer <enriquemarin_sierra@hotmail.com>
2021-12-19 22:17:57 +0100

    comments added to index.js

--- src/index.js
+++ src/index.js
@@ -8,3 +8,17 @@ ReactDOM.render(
     <CalendarApp />,
   document.getElementById('root')
 );
+
+/**
+ * How should I configure create-react-app to serve app from subdirectory?. VER:
+ * https://stackoverflow.com/questions/49429906/how-should-i-configure-create-react-app-to-serve-app-from-subdirectory
+ * 
+ * Building for Relative Paths. VER:
+ * https://create-react-app.dev/docs/deployment/#building-for-relative-paths
+ * https://v5.reactrouter.com/web/api/BrowserRouter/basename-string
+ * 
+ * git ignore .env files not working. VER:
+ * https://stackoverflow.com/questions/38983153/git-ignore-env-files-not-working
+ * 
+ * 
+ */
